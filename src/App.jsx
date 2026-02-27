515dc0dc7d3c46185a1a83d7344879d3273d6c79
Geethanga <dissanayakegeethanga@gmail.com>
2026-02-28 02:58:47 +0530

    add all starting files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AsliyaKallista from './asliya-kallista'
+
+const App = () => {
+  return (
+    <div>
+      <AsliyaKallista />
+    </div>
+  )
+}
+
+export default App
